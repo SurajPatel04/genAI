cfc3f9f8177ac4ce60f9e93456ae5193e3595390
SurajPatel04 <surajpatel9390@gmail.com>
2025-04-27 23:22:36 +0530

    routing

---
+++ cohort/routing/loder.py
@@ -0,0 +1,43 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY") 
+
+def loader(file_name, url_link, collection_name):
+    pdf_path = Path(__file__).parent/ file_name
+    loader = PyPDFLoader(pdf_path)
+    doc = loader.load()
+
+    text_split = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    split_doc = text_split.split_documents(documents=doc)
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/text-embedding-004",
+    )
+
+    vector_store = QdrantVectorStore.from_documents(
+        documents=[],
+        url=url_link,
+        embedding=embeddings,
+        collection_name=collection_name,
+    )
+
+    vector_store.add_documents(documents=split_doc)
+
+    print(f"this collection is added {collection_name} in the port {url_link}")
+
+loader("python.pdf","http://localhost:6333", "python")
+loader("nodejs.pdf","http://localhost:6335", "node js")
---
+++ cohort/routing/rag.py
@@ -0,0 +1,98 @@
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pathlib import Path
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+import os
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+load_dotenv()
+print("Chat With Your Doc\n")
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY") 
+
+
+""" here (__file__).parent meanse current dir mai hai file """
+pdf_path = Path(__file__).parent / "Matter.pdf"
+
+loader = PyPDFLoader(file_path=pdf_path)
+doc = loader.load()
+
+
+""" chunk_overlap means it has some previouse chunk part also """
+text_spliter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+split_doc = text_spliter.split_documents(documents=doc)
+
+
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/text-embedding-004",
+)
+
+vector_store = QdrantVectorStore.from_documents(
+     documents=[],
+     url="http://localhost:6333",
+     embedding=embeddings,
+     collection_name="learning_langchain"
+ )
+
+vector_store.add_documents(documents=split_doc)
+
+""" print("Injection Done") """
+
+
+retriver = QdrantVectorStore.from_existing_collection(
+    collection_name = "learning_langchain",
+    embedding=embeddings,
+    url="http://localhost:6333"
+)
+
+relevent_chunk = retriver.similarity_search(
+    query="What is Characteristics of Particles of Matter",
+
+)
+
+""" print(relevent_chunk) """
+
+formated = []
+
+for doc in relevent_chunk:
+    snippet = f"[Page {doc.metadata.get('page')}] \n{doc.page_content}"
+    formated.append(snippet)
+
+context = "\n\n".join(formated)
+
+client = OpenAI(
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+
+SYSTEM_PROMPT = """
+You are an helpfull AI Assiatant who responds base of the avilable context.
+
+Context: 
+{context}
+"""
+
+query = input("Ask Question: ")
+
+message = [{"role":"system","content":SYSTEM_PROMPT},{"role":"user","content":query}]
+
+response = client.chat.completions.create(
+            model="gemini-2.0-flash",
+            response_format={"type":"json_object"},
+            messages = message
+    )
+
+print("Answer is: \n\n")
+print(response.choices[0].message.content)
